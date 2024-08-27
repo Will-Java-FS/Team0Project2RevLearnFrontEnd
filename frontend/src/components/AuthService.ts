da6c1daf9198d131955a16f70f9ff161e490ff6f
gufe2213 <gufe2213@colorado.edu>
2024-08-27 14:07:53 -0600

    more semi colons

--- frontend/src/components/AuthService.ts
+++ frontend/src/components/AuthService.ts
@@ -6,7 +6,7 @@ class AuthService {
         sessionStorage.setItem("token", "Bearer " + token);
 
         console.log("User " + username + " logged in successfully");
-    }
+    };
 
     logout(): void {
         localStorage.clear();
@@ -14,31 +14,31 @@ class AuthService {
         window.location.reload();
 
         console.log("User logged out successfully");
-    }
+    };
 
 	loggedInUsername(): string {
         let username = sessionStorage.getItem("authenticatedUser");
-		if (username == null) {return "NO LOGGED IN USER";}
+		if (username == null) {return "NO LOGGED IN USER";};
         return username;
-	}
+	};
 
     loggedInUserId(): number {
         let id = sessionStorage.getItem("authenticatedUserId");
-        if (id == null) {return -1;}
+        if (id == null) {return -1;};
 		return Number(id);
-	}
+	};
 
     isLoggedIn(): boolean {
         return sessionStorage.getItem("role") == "student" || sessionStorage.getItem("role") == "teacher";
-    }
+    };
 
     isLoggedInStudent(): boolean {
         return sessionStorage.getItem("role") == "student";
-    }
+    };
 
     isLoggedInTeacher(): boolean {
         return sessionStorage.getItem("role") == "teacher";
-    }
+    };
 }
 
 export default new AuthService();
