119cf025e4ec6a3adbf3ac015ecaa12a94862d1a
Andrew T <me@andrewtran-shi.dev>
2024-08-27 12:38:41 -0700

    modified:   frontend/src/App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-<<<<<<< Updated upstream
-
-
 import Layout from './pages/Layout';
-=======
->>>>>>> Stashed changes
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Course from './pages/Course';
@@ -17,7 +12,6 @@ import AllPrograms from './pages/AllCourses';
 import AllCourses from './pages/AllCourses';
 import Forum from './pages/Forum';
 import ForumPost from './pages/ForumPost';
-        import Layout from './pages/Layout';
 
 const App = () => {
   const handleRegisterSuccess = () => {
@@ -27,58 +21,24 @@ const App = () => {
 
   return (
     <Router>
-
-<<<<<<< Updated upstream
       <Layout>
         <Routes>
-
-
-
-      {/* <Layout> */}
-        <Routes>
-          {/* <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-
           <Route
             path="/register"
             element={<Register onRegisterSuccess={handleRegisterSuccess} />}
           />
-
-          <Route path="/login" element={<Login onLoginSuccess={handleRegisterSuccess} />} />
+          <Route path="/login" element={<Login onRegisterSuccess={handleRegisterSuccess} />} />
           <Route path="/dashboard" element={<Dashboard />} />
-
-
-          <Route path="/login" element={<Login onRegisterSuccess={handleRegisterSuccess} />} /> */}
-          <Route path="/allprograms" element={<AllPrograms/>}/>
-          <Route path="/dashboard" element={<Dashboard/>}/>
-          <Route path="/about" element={<About />} />
+          <Route path="/allprograms" element={<AllPrograms />} />
           <Route path="/allcourses" element={<AllCourses />} />
-          <Route path="/course" element={<Course/>}/>
-          <Route path="/lesson" element={<Lesson/>}/>
-          <Route path="/forum" element={<Forum/>}/>
-          <Route path="/forumpost" element={<ForumPost/>}/>
+          <Route path="/course" element={<Course />} />
+          <Route path="/lesson" element={<Lesson />} />
+          <Route path="/forum" element={<Forum />} />
+          <Route path="/forumpost" element={<ForumPost />} />
         </Routes>
-                </Layout>
-      {/* </Layout> */}
-=======
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/register"
-          element={<Register onRegisterSuccess={handleRegisterSuccess} />}
-        />
-        <Route path="/login" element={<Login onRegisterSuccess={handleRegisterSuccess} />} />
-        <Route path="/allprograms" element={<AllPrograms />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/allcourses" element={<AllCourses />} />
-        <Route path="/course" element={<Course />} />
-        <Route path="/lesson" element={<Lesson />} />
-        <Route path="/forum" element={<Forum />} />
-        <Route path="/forumpost" element={<ForumPost />} />
-      </Routes>
->>>>>>> Stashed changes
-
+      </Layout>
     </Router>
   );
 };
