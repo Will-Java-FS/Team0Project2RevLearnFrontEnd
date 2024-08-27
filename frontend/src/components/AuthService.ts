4bcbd2dce01451bafac892effb33bd49f2603ecd
gufe2213 <gufe2213@colorado.edu>
2024-08-27 13:13:52 -0600

    Created services

---
+++ frontend/src/components/AuthService.ts
@@ -0,0 +1,44 @@
+class AuthService {
+    login(id:string, username:string, role:string, token:string): void {
+        sessionStorage.setItem("authenticatedUserId", id);
+        sessionStorage.setItem("authenticatedUser", username);
+        sessionStorage.setItem("role", role);
+        sessionStorage.setItem("token", "Bearer " + token);
+
+        console.log("User " + username + " logged in successfully");
+    }
+
+    logout(): void {
+        localStorage.clear();
+        sessionStorage.clear();
+        window.location.reload();
+
+        console.log("User logged out successfully");
+    }
+
+	loggedInUsername(): string {
+        let username = sessionStorage.getItem("authenticatedUser");
+		if (username == null) {return "NO LOGGED IN USER";}
+        return username;
+	}
+
+    loggedInUserId(): number {
+        let id = sessionStorage.getItem("authenticatedUserId");
+        if (id == null) {return -1;}
+		return Number(id);
+	}
+
+    isLoggedIn(): boolean {
+        return sessionStorage.getItem("role") == "student" || sessionStorage.getItem("role") == "teacher";
+    }
+
+    isLoggedInStudent(): boolean {
+        return sessionStorage.getItem("role") == "student";
+    }
+
+    isLoggedInTeacher(): boolean {
+        return sessionStorage.getItem("role") == "teacher";
+    }
+}
+
+export default new AuthService();
---
+++ frontend/src/components/Redirector.ts
@@ -0,0 +1,16 @@
+import  { useNavigate } from 'react-router-dom'
+import AuthService from "./AuthService";
+
+export default function Redirector() {
+    const navigate = useNavigate();
+
+    if (AuthService.isLoggedInStudent()) {
+        navigate('/dashboard')
+    }
+    else if (AuthService.isLoggedInTeacher()) {
+        navigate('/dashboard')
+    } 
+    else {
+        navigate('/home')
+    }
+}
