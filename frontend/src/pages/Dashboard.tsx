4e70db4790be566e46380b1cec751cd7e1fa704f
azhou0770 <azhou0770@gmail.com>
2024-08-28 13:06:36 -0700

    Added some cards in the dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -15,8 +15,32 @@ export default function Dashboard(){
     return (
         <>
             <h1>Dashboard</h1>
-            <h3>List of Programs</h3>
-            <h3>Progress Tracker</h3>
+            <h3>Your Programs</h3>
+            <div className="card bg-base-200 w-500 shadow-xl">
+            <div className="card-body flex flex-row items-center justify-between">
+                <h2 className="card-title">Infosys V2 Java  Program</h2>
+                <div className="card-actions">
+                <button className="btn btn-primary">Click here</button>
+                </div>
+            </div>
+            </div>
+            <div className="card bg-base-200 w-500 shadow-xl">
+            <div className="card-body flex flex-row items-center justify-between">
+                <h2 className="card-title">asdf V2 Java  Program</h2>
+                <div className="card-actions">
+                <button className="btn btn-primary">Click here</button>
+                </div>
+            </div>
+            </div>
+            <div className="card bg-base-200 w-500 xl">
+            <div className="card-body flex flex-row items-center justify-between">
+                <h2 className="card-title">asdf V2 Java  Program</h2>
+                <div className="card-actions">
+                <button className="btn btn-primary">Click here</button>
+                </div>
+            </div>
+            </div>
+            <h2>Progress Tracker</h2>
 
         </>
     )
