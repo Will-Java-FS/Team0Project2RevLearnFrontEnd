6c71a43f3abd3332536b1d617981f7239530ef98
azhou0770 <azhou0770@gmail.com>
2024-08-28 12:23:00 -0700

    Implemented components for lesson.tsx

--- frontend/src/pages/Lesson.tsx
+++ frontend/src/pages/Lesson.tsx
@@ -13,19 +13,120 @@ export default function Lesson(){
             .catch(error => console.error('Error fetching lesson:', error));
     }, []);
 
-    if (!lesson) {
-        return <p>Loading lesson...</p>;
-    }
+    // if (!lesson) {
+    //     return <p>Loading lesson...</p>;
+    // }
 
     return (
         <>
+            <h1>Lesson Title</h1>
+
             {/* <h1>{lesson.title}</h1>
             <p>{lesson.description}</p> */}
-            <h1>Lesson Title</h1>
-            <h3>Description</h3>
-            <p>lorem ipsum</p>
-            <h3>Implementation</h3>
-            <p>lorem ipsum 123</p>
+            <div className="collapse collapse-arrow bg-base-200">
+                <input type="radio" name="my-accordion-2" defaultChecked />
+                <div className="collapse-title text-xl font-medium">Prerequisites and Learning Objectives</div>
+                <div className="collapse-content">
+                    <p>lorem ipsum 123</p>
+                </div>
+            </div>
+            <div className="collapse collapse-arrow bg-base-200">
+                <input type="radio" name="my-accordion-2" />
+                <div className="collapse-title text-xl font-medium">Description</div>
+                <div className="collapse-content">
+                    <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Fames natoqu sollicitudin.</p>
+                </div>
+            </div>
+            <div className="collapse collapse-arrow bg-base-200">
+                <input type="radio" name="my-accordion-2" />
+                <div className="collapse-title text-xl font-medium">Real World Application</div>
+                <div className="collapse-content">
+                    <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Fames natoqu sollicitudin.</p>
+                </div>
+            </div>
+            <div className="collapse collapse-arrow bg-base-200">
+                <input type="radio" name="my-accordion-2" />
+                <div className="collapse-title text-xl font-medium">Implementation</div>
+                <div className="collapse-content">
+                    <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Fames natoqu sollicitudin.</p>
+                </div>
+            </div>
+            <div className="collapse collapse-arrow bg-base-200">
+                <input type="radio" name="my-accordion-2" />
+                <div className="collapse-title text-xl font-medium">Summary</div>
+                <div className="collapse-content">
+                    <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Fames natoqu sollicitudin.</p>
+                </div>
+            </div>
+
+            <div>
+                <button className="btn btn-active">Complete Lesson</button>
+            </div>
+
+
+            <div>
+                <h1>Create a Lesson</h1>
+                <label className="form-control w-full max-w-xs">
+                <div className="label">
+                    <span className="label-text">What is the lesson title?</span>
+                </div>
+
+                <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                <div className="label">
+                </div>
+                </label>
+
+                <label className="form-control w-full max-w-xs">
+                <div className="label">
+                    <span className="label-text">What are the prerequisites and learning objectives?</span>
+                </div>
+                <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                <div className="label">
+                </div>
+                </label>
+
+                <label className="form-control w-full max-w-xs">
+                <div className="label">
+                    <span className="label-text">What is the description ?</span>
+                </div>
+                <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                <div className="label">
+                </div>
+                </label>
+
+                <label className="form-control w-full max-w-xs">
+                <div className="label">
+                    <span className="label-text">What is the Real World Application ?</span>
+                </div>
+                <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                <div className="label">
+                </div>
+                </label>
+
+                <label className="form-control w-full max-w-xs">
+                <div className="label">
+                    <span className="label-text">What is the Implementation ?</span>
+                </div>
+                <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                <div className="label">
+                </div>
+                </label>
+
+                <label className="form-control w-full max-w-xs">
+                <div className="label">
+                    <span className="label-text">What is the Summary ?</span>
+                </div>
+                <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                <div className="label">
+                </div>
+                </label>
+                <div>
+                    <button className="btn btn-active">Create Lesson</button>
+                </div>
+            </div>
+
+
+
         </>
     )
 }
