0c339f8750165e8aa2fa25cac540312ac28cb1fe
gufe2213 <gufe2213@colorado.edu>
2024-09-09 10:13:45 -0600

    Filled out about page

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,15 +1,60 @@
-import Card from "../components/Card";
-
 // Show all info about the project (what it is, who made it, ect)
-// Use authService to get logged in user's info, use the axios services for http requests
 export default function About() {
   return (
-    <div className="flex justify-center items-center min-h-screen  p-6">
-      <Card
-        title="Tailwind Card"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc felis ligula."
-        link="https://example.com"
-      />
+    <div className="min-h-screen w-full flex">
+        {/* Match home page background */}
+        <section
+            className="hero glass min-h-screen bg-gradient-to-r from-amber-200 via-orange-400 to-red-600 dark:from-yellow-500 dark:via-orange-500 dark:to-red-500
+            sm:bg-gradient-to-r sm:from-amber-200 sm:via-orange-400 sm:to-red-600
+            dark:sm:from-yellow-500 dark:sm:via-orange-500 dark:sm:to-red-500
+            md:bg-gradient-to-r md:from-amber-200 md:via-orange-400 md:to-red-600
+            dark:md:from-yellow-500 dark:md:via-orange-500 dark:md:to-red-500
+            lg:bg-gradient-to-r lg:from-amber-200 lg:via-orange-400 lg:to-red-600
+            dark:lg:from-yellow-500 dark:lg:via-orange-500 dark:lg:to-red-500"
+        >
+            <div className="hero-content text-center text-white">
+                <div className="max-w-xl mx-auto">
+                    <h1 className="drop-shadow-xl py-4 text-5xl font-bold">
+                      Revature Training Capstone Project - RevLearn
+                    </h1>
+                    <h2 className="drop-shadow-xl text-3xl py-2 font-bold">Team Two Members:</h2>
+                    <div className="drop-shadow-xl text-lg">
+                      <p>Andrew Tran Shi</p>
+                      <p>Anthony Zhou</p>
+                      <p>Anup Jacob</p>
+                      <p>Egan Okumu</p>
+                      <p>Emmett Duffy</p>
+                      <p>Gunnar Fettig</p>
+                      <p>Matthew Darrow</p>
+                      <p>Oscar Ramirez</p>
+                      <p>Radhika Godhasara</p>
+                      <p>Ruar Podushkin</p>
+                      <p>Ryan Schwartz</p>
+                      <p>Sarah Anatoli</p>
+                      <p>Stephen Becker</p>
+                    </div>
+                    <h2 className="drop-shadow-xl text-3xl py-2 font-bold">About This Project:</h2>
+                    <div className="drop-shadow-xl text-lg font-light">
+                      <p>This project was started on the 26th of August 2024.</p>
+                      <p>It was completed on the 10th of September 2024.</p>
+                      <p>
+                          The project consists of a learning platform where students can
+                          enroll in programs and complete courses in various subjects. It
+                          also has functionality to allow teachers to create and manage
+                          programs, courses and lessons.
+                      </p>
+                      
+                    </div>
+                    <h2 className="drop-shadow-xl text-3xl py-2 font-bold">Technology Used:</h2>
+                    <div className="drop-shadow-xl text-lg font-light">
+                      <p>Backend - Java, Spring, Kafka, Postgresql</p>
+                      <p>Frontend - React, HTML, Typescript, Vite, TailwindCSS, DaisyUI</p>
+                      <p>DevOps - AWS (EC2, S3, RDS), Jenkins</p>
+                      <p>Version Control - Git, GitHub</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
   );
 }
