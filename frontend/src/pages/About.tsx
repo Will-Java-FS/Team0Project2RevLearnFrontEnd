92dae2f7ed6eaabb5b937210d53b909eceba9e3a
gufe2213 <gufe2213@colorado.edu>
2024-09-11 10:34:34 -0600

    Minor alignment change

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,10 +1,10 @@
 // Show all info about the project (what it is, who made it, ect)
 export default function About() {
   return (
-    <div className="min-h-screen w-full text-center flex">
+    <div className="min-h-screen w-full justify-center items-center text-center flex">
         {/* Match home page background */}
         <section
-            className="hero glass min-h-screen w-full bg-gradient-to-r from-amber-200 via-orange-400 to-red-600 dark:from-yellow-500 dark:via-orange-500 dark:to-red-500
+            className="glass min-h-screen w-full bg-gradient-to-r from-amber-200 via-orange-400 to-red-600 dark:from-yellow-500 dark:via-orange-500 dark:to-red-500
             sm:bg-gradient-to-r sm:from-amber-200 sm:via-orange-400 sm:to-red-600
             dark:sm:from-yellow-500 dark:sm:via-orange-500 dark:sm:to-red-500
             md:bg-gradient-to-r md:from-amber-200 md:via-orange-400 md:to-red-600
@@ -12,8 +12,9 @@ export default function About() {
             lg:bg-gradient-to-r lg:from-amber-200 lg:via-orange-400 lg:to-red-600
             dark:lg:from-yellow-500 dark:lg:via-orange-500 dark:lg:to-red-500"
         >
-            <div className="hero-content w-full text-base-100">
+            <div className="w-full text-base-100">
                 <div className="max-w-xl mx-auto">
+                  <br/>
                     <h1 className="drop-shadow-xl py-4 text-4xl font-bold">
                       Revature Training Capstone Project - RevLearn
                     </h1>
@@ -37,6 +38,7 @@ export default function About() {
                       <p>Sarah Anatoli</p>
                       <p>Stephen Becker</p>
                     </div>
+                    <br/>
                     <h2 className="drop-shadow-xl text-3xl py-2 font-bold">About This Project:</h2>
                     <div className="drop-shadow-xl text-lg font-light">
                       <p>This project was started on the 26th of August 2024</p>
@@ -50,6 +52,7 @@ export default function About() {
                           moderate.
                       </p>
                     </div>
+                    <br/>
                     <h2 className="drop-shadow-xl text-3xl py-2 font-bold">Technologies Used:</h2>
                     <div className="drop-shadow-xl text-lg font-light">
                       <p>Backend - Java, Spring, Kafka, Postgresql</p>
