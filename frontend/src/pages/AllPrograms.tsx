300e119beea95cae513b931f0ea369f1b7c99c32
Andrew Tran-Shi <me@andrewtran-shi.dev>
2024-09-11 07:32:43 -0700

    modified:   frontend/src/pages/AllPrograms.tsx

--- frontend/src/pages/AllPrograms.tsx
+++ frontend/src/pages/AllPrograms.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axiosInstance from "../components/AxiosConfig";
 import AuthService from "../components/AuthService";
 
