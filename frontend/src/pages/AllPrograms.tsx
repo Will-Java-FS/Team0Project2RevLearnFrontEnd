3f6e65cdf2fd4135b2ca9789e504f8c1432b9e63
gufe2213 <gufe2213@colorado.edu>
2024-09-11 14:10:42 -0600

    Highlight users program

--- frontend/src/pages/AllPrograms.tsx
+++ frontend/src/pages/AllPrograms.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import AuthService from "../components/AuthService";
 
 interface Program {
   programId: number;
@@ -23,19 +24,40 @@ export default function AllPrograms() {
     fetchPrograms();
   }, []);
 
+  function Item({ programId, programName }: { programId:number, programName:string }) {
+    if (programId == AuthService.getLoggedInUserProgramId()) {
+      return (
+        <div
+            key={programId}
+            className="card bg-base-200 shadow-xl border-2 border-primary"
+          >
+            <div className="card-body">
+              <h2 className="card-title">{programName} - Your program</h2>
+            </div>
+          </div>
+      );
+    }
+    return (
+      <div
+            key={programId}
+            className="card bg-base-100 shadow-xl border-2 border-accent"
+          >
+            <div className="card-body">
+              <h2 className="card-title">{programName}</h2>
+            </div>
+          </div>
+    );
+  }
+
   return (
     <div className="bg-base-100 p-5">
       <h1 className="text-2xl font-bold mb-4">List of Programs</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {programs.map((program) => (
-          <div
-            key={program.programId}
-            className="card bg-base-100 shadow-xl border-2 border-primary"
-          >
-            <div className="card-body">
-              <h2 className="card-title">{program.programName}</h2>
-            </div>
-          </div>
+          <Item
+            programId={program.programId}
+            programName={program.programName}
+          />
         ))}
       </div>
     </div>
